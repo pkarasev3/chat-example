29cf60ab2760eb1a1e8cb7cf4d964694deb3aec6
peter karasev <peter.karasev@keysight.com>
2017-11-08 11:09:31 -0500

    minor edits for understanding of grbg

--- index.js
+++ index.js
@@ -9,10 +9,19 @@ app.get('/', function(req, res){
 
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+    io.emit('chat message', msg);        
+    console.log('sending message: ' + msg)        
   });
+  console.log('some clown connected...');
+  var interval = setInterval(function(str1, str2) {
+  socket.emit( str1 + str2 );
+  console.log('grbg');
+    }, 200, "Hello.", "How are you?");
 });
 
 http.listen(port, function(){
   console.log('listening on *:' + port);
 });
+
+
+
